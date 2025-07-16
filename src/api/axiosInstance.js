eb112aeaad71a724129eb47d60eef5eb9337f192
Sahil Barak <111504005+sahil0989@users.noreply.github.com>
2025-07-16 22:25:45 +0530

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://goskilled-backed-2-0.onrender.com",
 });
 
 export default axiosInstance;
